bb018047fe1a225a3696110f760e2fd76d4bd569
sbejksen383992g-hash <sbejksen383992g@gmail.com>
2026-01-16 12:43:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 from datetime import date
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is alive"
+
+if __name__ == "__main__":
+    app.run()
+
 
 app = Flask(__name__)
 CORS(app)
@@ -415,5 +426,6 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
